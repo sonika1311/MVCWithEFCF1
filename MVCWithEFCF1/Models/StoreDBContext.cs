a5cab050f78fd94b85f7963cecd1339aff274b41
sonika1311 <sonikakumaribom@gmail.com>
2024-08-06 16:02:39 +0530

    db context file and webconfig

---
+++ MVCWithEFCF1/Models/StoreDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithEFCF1.Models
+{
+    public class StoreDBContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
